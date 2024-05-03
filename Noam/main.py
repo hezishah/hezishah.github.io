8d18052bf5b236757d29fa201d9b8ac11fba43bc
Hezi Shahmoon <hshahmoon@apple.com>
2024-05-03 21:23:26 +0300

    Update to new Excel file
    Signed-off-by: Hezi Shahmoon <hezishah@gmain.com>

--- Noam/main.py
+++ Noam/main.py
@@ -2,9 +2,9 @@ import pandas
 import json
 
 def main():
-    book = pandas.read_excel("רשימת_מילים.xlsx")
+    book = pandas.read_excel("words_v2.xlsx")
     d = book.to_dict('records')
-    with open("words.json", "w") as outfile: 
+    with open("words_v2.json", "w") as outfile: 
         json.dump(d, outfile)
 if __name__ == "__main__":
     main()
