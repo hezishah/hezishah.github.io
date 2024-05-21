57b6217fafe3a77f07f7fa1efc60341262320ffe
Hezi Shahmoon <hshahmoon@apple.com>
2024-05-21 11:07:45 +0300

    Update Noam's words and logic
    Signed-off-by: Hezi Shahmoon <hezishah@gmail.com>

--- Noam/main.py
+++ Noam/main.py
@@ -2,9 +2,9 @@ import pandas
 import json
 
 def main():
-    book = pandas.read_excel("words_v4.xlsx")
+    book = pandas.read_excel("words_v5.xlsx")
     d = book.to_dict('records')
-    with open("words_v2.json", "w") as outfile: 
+    with open("words_v5.json", "w") as outfile: 
         json.dump(d, outfile)
 if __name__ == "__main__":
     main()
